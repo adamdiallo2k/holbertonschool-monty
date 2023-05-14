4bfb8009f73cd7d439f15fc8300e96f1628f7f2a
AdamDiallo <adamdiallo2k@gmail.com>
2023-05-14 23:08:58 +0200

    task nop

--- monty.h
+++ monty.h
@@ -52,4 +52,7 @@ void pop(stack_t **top, unsigned int line_number);
 void nop(stack_t **top, unsigned int line_number);
 void swap(stack_t **top, unsigned int line_number);
 void add(stack_t **top, unsigned int line_number);
+void pint(stack_t **top, unsigned int line_number);
+void nop(stack_t **top, unsigned int line_number);
+
 #endif /*MONTY_H*/
